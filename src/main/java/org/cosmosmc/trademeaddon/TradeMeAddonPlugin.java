f44fa363f8ada7e04830e14c3cc7af0753a0e7ec
Michael <whitefoxpvp@gmail.com>
2025-01-18 20:08:22 -0500

    Initial commit

---
+++ src/main/java/org/cosmosmc/trademeaddon/TradeMeAddonPlugin.java
@@ -0,0 +1,17 @@
+package org.cosmosmc.trademeaddon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TradeMeAddonPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
